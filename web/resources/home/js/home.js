a03f6a40228ad75c6025c8606ae30ce4fdb8b72f
Douglas Fernandes <br.com.age@gmail.com>
2018-01-12 13:48:23 -0300

    [Imobille v1.0] - Douglas Fernandes: Atualização de front

--- web/resources/home/js/home.js
+++ web/resources/home/js/home.js
@@ -5,7 +5,7 @@ function applySlicker() {
         infinite: true,
         speed: 1000,
         slidesToShow: 3,
-        slidesToScroll: 3,
+        slidesToScroll: 1,
         variableWidth: true,
         autoplay: true,
         autoplaySpeed: 3000,
@@ -13,8 +13,8 @@ function applySlicker() {
           {
             breakpoint: 1024,
             settings: {
-              slidesToShow: 3,
-              slidesToScroll: 3,
+              slidesToShow: 2,
+              slidesToScroll: 1,
               dots: true,
               centerMode: true
             }
